c2c2c3f28833bd5aedfe35ab2ba898384500e19b
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2016-11-22 11:17:47 -0800

    Delta computation

--- csv.py
+++ csv.py
@@ -4,12 +4,10 @@ import numpy as np
 rgb = cv2.imread("render_0_rgb.png")
 parts = cv2.imread("render_0_parts.png")
 
-cv2.imshow("parts", parts)
-
 def skin(rgb):
     channels = cv2.split(rgb)
     out = (channels[2] * 0.6 - channels[1] * 0.3 - channels[0] * 0.3) - 10
-    return out
+    return (out > 0)*0
 
 def foreground(rgb):
     channels = cv2.split(rgb)
@@ -20,14 +18,30 @@ def findPart(parts, color):
     delta = np.array([4, 4, 4])
     return cv2.inRange(parts, color - delta, color + delta)
 
-cv2.imshow("skin", skin(rgb))
-cv2.imshow("fg", foreground(rgb))
-cv2.imshow("head", findPart(parts, np.array([0x00, 0x00, 0x5B])))
+def index(mat, pt):
+    return mat[pt[0], pt[1]]
 
 def delta(mats, weights, pt, u, v):
-    print(pt + u)
-    print(pt + v)
+    (rgb, skin, foreground) = mats
+    (left, right) = (pt + u, pt + v)
 
-delta(None, None, np.array([100, 100]), np.array([50, 50]), np.array([-50, 50]))
+    colorScore = index(rgb, left)**2        - index(rgb, right)**2
+    skinScore  = index(skin, left)**2       - index(skin, right)**2
+    fgScore    = index(foreground, left)**2 - index(foreground, right)**2
 
-cv2.waitKey(0)
+    print(np.dot(np.array([1,2,3]), np.array([2,3,4])))
+    return np.dot(np.array([colorScore, skinScore, fgScore]), weights)
+
+d = delta((cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY), skin(rgb), foreground(rgb)),
+        np.array([1, 10, 100]),
+        np.array([500, 500]),
+        np.array([-8, 0]), np.array([0, 0]))
+
+print(d)
+
+# cv2.imshow("parts", parts)
+# cv2.imshow("skin", skin(rgb))
+# cv2.imshow("fg", foreground(rgb))
+# cv2.imshow("head", findPart(parts, np.array([0x00, 0x00, 0x5B])))
+
+# cv2.waitKey(0)
