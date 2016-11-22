f1cc949c5d02b05fd07e28febb7b20aab8f2f7ea
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2016-11-22 12:47:12 -0800

    Generate csv's to play with

--- csv.py
+++ csv.py
@@ -1,3 +1,4 @@
+import random, math
 import cv2
 import numpy as np
 
@@ -17,23 +18,31 @@ def findPart(parts, color):
     delta = np.array([4, 4, 4])
     return cv2.inRange(parts, color - delta, color + delta)
 
+def isPart(parts, pt, color):
+    c = index(parts, pt)
+    delta = np.array([4, 4, 4])
+    lower = color - delta
+    upper = color + delta
+    return 1 * (np.all(c > lower) and np.all(c < upper))
+
 def index(mat, pt):
     apt = pt
 
     if pt[0] < 0:
         apt[0] = 0
-    elif pt[0] > np.size(mat, 1):
-        apt[0] = mat[pt]
+    elif pt[0] >= np.size(mat, 1):
+        apt[0] = np.size(mat, 1) - 1
         
     if pt[1] < 0:
         apt[1] = 0
-    elif pt[1] > np.size(mat, 0):
-        apt[1] = mat[pt]
+    elif pt[1] >= np.size(mat, 1):
+        apt[1] = np.size(mat, 1) - 1
 
     return mat[apt[0], apt[1]]
 
-def delta(mats, weights, pt, u, v):
-    (rgb, skin, foreground) = mats
+def delta(mats, weights, pt, offset):
+    (u, v) = offset
+    (rgb, skin, foreground, _) = mats
     (left, right) = (pt + u, pt + v)
 
     colorScore = index(rgb, left)**2        - index(rgb, right)**2
@@ -44,15 +53,47 @@ def delta(mats, weights, pt, u, v):
 
 def process(number):
     rgb = cv2.imread(prefix + str(number) + "_rgb.png")
-    return (cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY), skin(rgb), foreground(rgb))
+    parts = cv2.imread(prefix + str(number) + "_parts.png")
+    return (cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY), skin(rgb), foreground(rgb), parts)
 
+def randoffset():
+    sd = 10
+    return np.array([int(random.gauss(0, sd)), int(random.gauss(0, sd))])
 
-d = delta(process(0),
-        np.array([1, 10, 100]),
-        np.array([500, 500]),
-        np.array([-8, 0]), np.array([0, 0]))
+def randvec(_):
+    return (randoffset(), randoffset())
 
-print(d)
+def randpoint(img):
+    return np.array([int(random.uniform(0, np.size(img, 0))),
+        int(random.uniform(0, np.size(img, 1)))])
+
+DELTA_WEIGHTS = np.array([1, 10, 100])
+
+def sample():
+    features = 10
+    print("isHead," + ",".join(map(lambda q: "d" + str(q), xrange(1, features))))
+    offsets = map(randvec, [None] * features)
+
+    img = process(0)
+
+    for i in range(1, 10000):
+        pt = randpoint(img[0])
+
+        out = [isPart(img[3], pt, np.array([0x00, 0x00, 0x5B]))]
+
+        for off in offsets:
+            out.append(delta(img, DELTA_WEIGHTS, pt, off))
+
+        print(",".join(map(str, out)))
+
+#d = delta(process(0),
+#        DELTA_WEIGHTS,
+#        np.array([500, 500]),
+#        randvec())
+
+#print(d)
+
+sample()
 
 # cv2.imshow("parts", parts)
 # cv2.imshow("skin", skin(rgb))
