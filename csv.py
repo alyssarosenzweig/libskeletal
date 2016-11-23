4bd7b9e05a08bcf63d5cd0bcb9b0c1cccc8dbb24
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2016-11-22 18:14:53 -0800

    Furth experiments

--- csv.py
+++ csv.py
@@ -3,17 +3,18 @@ import cv2
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier 
 from sklearn import linear_model
+from sklearn.naive_bayes import GaussianNB, BernoulliNB
 
 prefix = "/home/alyssa/synthposes/dataset/render_"
 
 def skin(rgb):
     channels = cv2.split(rgb)
     out = (channels[2] * 0.6 - channels[1] * 0.3 - channels[0] * 0.3) - 10
-    return (out > 0)*0
+    return (out > 0) * 1
 
 def foreground(rgb):
     channels = cv2.split(rgb)
-    (r, d) = cv2.threshold(channels[0] - 0x8C, 0, 255, cv2.THRESH_BINARY)
+    (r, d) = cv2.threshold(channels[0] - 0x8C, 0, 1, cv2.THRESH_BINARY)
     return d
 
 def findPart(parts, color):
@@ -42,16 +43,15 @@ def index(mat, pt):
 
     return mat[apt[0], apt[1]]
 
-def delta(mats, weights, pt, offset):
+def gammamat(mats):
+    (gray, skin, foreground, _) = mats
+    return gray*1 + skin*10 + foreground*100;
+
+def delta(gamma, pt, offset):
     (u, v) = offset
-    (rgb, skin, foreground, _) = mats
     (left, right) = (pt + u, pt + v)
 
-    colorScore = index(rgb, left)**2        - index(rgb, right)**2
-    skinScore  = index(skin, left)**2       - index(skin, right)**2
-    fgScore    = index(foreground, left)**2 - index(foreground, right)**2
-
-    return np.dot(np.array([colorScore, skinScore, fgScore]), weights)
+    return index(gamma, left) - index(gamma, right)
 
 def process(number):
     rgb = cv2.imread(prefix + str(number) + "_rgb.png")
@@ -71,11 +71,11 @@ def randpoint(img):
 
 DELTA_WEIGHTS = np.array([1, 10, 100])
 
-def sample(img, pt, offsets):
+def sample(gamma, pt, offsets):
     out = []
 
     for off in offsets:
-        out.append(delta(img, DELTA_WEIGHTS, pt, off))
+        out.append(delta(gamma, pt, off))
 
     return out
 
@@ -83,18 +83,20 @@ def train(features):
     offsets = map(randvec, [None] * features)
 
     img = process(0)
+    gamma = gammamat(img)
 
     X = []
     Y = []
 
-    for i in range(1, 1000):
+    for i in range(1, 50000):
         pt = randpoint(img[0])
 
         Y.append(isPart(img[3], pt, np.array([0x00, 0x00, 0x5B])))
-        X.append(sample(img, pt, offsets))
+        X.append(sample(gamma, pt, offsets))
 
-    clf = RandomForestClassifier(n_estimators=3)
-    #clf = linear_model.LogisticRegression()
+    #clf = RandomForestClassifier(n_estimators=10)
+    clf = linear_model.LogisticRegression()
+    #clf = GaussianNB()
     clf = clf.fit(X, Y)
 
     return (clf, offsets)
@@ -104,10 +106,11 @@ def visualize(model):
 
     img = process(1)
     vis = img[0].copy()
+    gamma = gammamat(img)
 
     for x in range(50, 1023):
         for y in range(400, 600):
-            s = sample(img, np.array([x, y]), offsets)
+            s = sample(gamma, np.array([x, y]), offsets)
             vis[x, y] = clf.predict(np.array(s).reshape(1, -1)) * 255
         cv2.imshow("Vis", vis)
         cv2.waitKey(2)
@@ -116,7 +119,7 @@ def visualize(model):
     return vis
 
 print("Training...")
-model = train(500)
+model = train(100)
 print("Running...")
 cv2.imshow("Visualization", visualize(model))
 cv2.waitKey(0)
